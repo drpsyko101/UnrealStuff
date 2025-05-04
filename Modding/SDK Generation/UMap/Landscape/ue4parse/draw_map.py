d0d8072f524698c100f56a1ed28d0ad22881029a
drpsyko101 <drpsyko101@gmail.com>
2025-05-04 22:37:26 +0800

    Add draw landscape splines

---
+++ Modding/SDK Generation/UMap/Landscape/ue4parse/draw_map.py
@@ -0,0 +1,189 @@
+import os
+import argparse
+import json
+import jsonpickle
+import drawsvg as draw
+from dacite import from_dict, Config
+from export_classes import (
+    AssetRef,
+    LandscapeSplinesComponent,
+    LandscapeSplineSegment,
+    SplineMeshComponent,
+    Vector2D,
+)
+
+
+class DrawSplines:
+    input_path = ""
+    image_size = Vector2D(0, 0)
+    bound_min = Vector2D(float("inf"), float("inf"))
+    bound_max = Vector2D(-float("inf"), -float("inf"))
+    components: list[LandscapeSplinesComponent] = []
+    segments: list[LandscapeSplineSegment] = []
+    spline_meshes: list[SplineMeshComponent] = []
+    output_path = ""
+
+    def __init__(self, input_path: str, output_path="", image_size="256x256") -> None:
+        self.input_path = input_path
+        if output_path:
+            self.output_path = output_path
+        else:
+            self.output_path = input_path.replace(".json", ".svg")
+        self.image_size = Vector2D(*list(map(float, image_size.split("x"))))
+        pass
+
+    @staticmethod
+    def convert_key_explicit(key: str) -> str:
+        match key:
+            case "Connections":
+                return "ConnectionStart"
+            case "Connections[1]":
+                return "ConnectionEnd"
+        return key
+
+    def process(self):
+        with open(self.input_path, encoding="utf8") as file:
+            data = json.load(file)
+
+        # parse data
+        for obj in data:
+            if obj["Type"] == "LandscapeSplinesComponent":
+                component = from_dict(LandscapeSplinesComponent, obj)
+                self.components.append(component)
+
+            if obj["Type"] == "LandscapeSplineSegment":
+                segment = from_dict(
+                    LandscapeSplineSegment,
+                    obj,
+                    Config(convert_key=self.convert_key_explicit),
+                )
+                self.segments.append(segment)
+
+            if obj["Type"] == "SplineMeshComponent":
+                spline_mesh = from_dict(
+                    SplineMeshComponent,
+                    obj,
+                )
+                self.spline_meshes.append(spline_mesh)
+
+        # Replace AssetRef with valid LandscapeSplineSegment
+        for component in self.components:
+            for index, seg in enumerate(component.Properties.Segments):
+                if isinstance(seg, AssetRef):
+                    for segment in self.segments:
+                        if seg.ObjectName.endswith(f"{segment.Name}'"):
+                            component.Properties.Segments[index] = segment
+                            for point in segment.Properties.SplineInfo.Points:
+                                loc = (
+                                    point.OutVal + component.Properties.RelativeLocation
+                                )
+                                self.bound_min.X = min(self.bound_min.X, loc.X)
+                                self.bound_min.Y = min(self.bound_min.Y, loc.Y)
+                                self.bound_max.X = max(self.bound_max.X, loc.X)
+                                self.bound_max.Y = max(self.bound_max.Y, loc.Y)
+                            break
+
+        # Replace AssetRef with valid SplineMeshComponent
+        for segment in self.segments:
+            for index, mesh in enumerate(segment.Properties.LocalMeshComponents):
+                if isinstance(mesh, AssetRef):
+                    for spline_mesh in self.spline_meshes:
+                        if mesh.ObjectName.endswith(f"{spline_mesh.Name}'"):
+                            segment.Properties.LocalMeshComponents[index] = spline_mesh
+                            break
+
+        paths = []
+        processed = 0
+
+        print("Bounds:", self.bound_min, self.bound_max)
+        self.image_size = self.bound_max - self.bound_min
+        canvas = draw.Drawing(self.image_size.X, self.image_size.Y, origin="top-left")
+
+        for component in self.components:
+            # print("Processing", component.Outer)
+            coords = []
+            path = draw.Path(stroke_width=1000, stroke="white", opacity=0.2)
+            for index, segment in enumerate(component.Properties.Segments):
+                if isinstance(segment, LandscapeSplineSegment):
+                    no_coll = all(
+                        (
+                            isinstance(mesh, SplineMeshComponent)
+                            and mesh.Properties.BodyInstance is None
+                        )
+                        for mesh in segment.Properties.LocalMeshComponents
+                    )
+                    if index == 0:
+                        canvas.append(
+                            draw.Text(
+                                str(component.Outer),
+                                1800,
+                                *(
+                                    segment.Properties.SplineInfo.Points[0].OutVal
+                                    + component.Properties.RelativeLocation
+                                    - self.bound_min
+                                ).ToVector2D(),
+                                fill="yellow",
+                                text_anchor="middle",
+                                center=True,
+                            )
+                        )
+                    for loc_index, point in enumerate(
+                        segment.Properties.SplineInfo.Points
+                    ):
+                        loc = (
+                            point.OutVal
+                            + component.Properties.RelativeLocation
+                            - self.bound_min
+                        )
+                        loc_2d = loc.ToVector2D()
+                        # print(loc_2d)
+                        coords.append(loc_2d)
+                        if loc_index == 0:
+                            path.M(*loc_2d)
+                        else:
+                            path.L(*loc_2d)
+                        canvas.append(
+                            draw.Text(
+                                str(index),
+                                1800,
+                                *loc_2d,
+                                fill="red",
+                                text_anchor="middle",
+                                center=True,
+                            )
+                        )
+            canvas.insert(1, path)
+            processed += 1
+
+        # print(jsonpickle.encode(coords, unpicklable=False))
+        canvas.extend(paths)
+        target_size = 256.0
+        scale_fac = target_size / self.image_size.Y
+        canvas.set_render_size(self.image_size.X * scale_fac, target_size)
+        canvas.save_svg(self.output_path)
+        print(f"Generated {processed} components")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="UE landscape SVG exporter")
+    parser.add_argument(
+        "--input", "-i", type=str, required=True, help="Path to the map JSON file"
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        required=False,
+        help="SVG image output path",
+    )
+    parser.add_argument(
+        "--dimension",
+        "-d",
+        type=str,
+        required=False,
+        default="256x256",
+        help="SVG image output dimension",
+    )
+    args = parser.parse_args()
+    ds = DrawSplines(args.input, output_path=args.output, image_size=args.dimension)
+    ds.process()
--- Modding/SDK Generation/UMap/Landscape/ue4parse/export_classes.py
+++ Modding/SDK Generation/UMap/Landscape/ue4parse/export_classes.py
@@ -1,39 +1,101 @@
-
 from dataclasses import dataclass, field
 import json
 from typing import Optional
 from numpy import asarray
 import numpy as np
+from enum import Enum
+import re
+
 
 @dataclass
 class Vector2D:
     X: float
     Y: float
+
     # ndarr: np.ndarray = np.ndarray((0,4), np.float32)
     def __post_init__(self):
         self.ndarr = asarray(list(self.__dict__.values()))
+
+    def __add__(self, B):
+        if not isinstance(B, Vector2D):
+            raise TypeError
+        return Vector2D(self.X + B.X, self.Y + B.Y)
+
+    def __sub__(self, B):
+        if not isinstance(B, Vector2D):
+            raise TypeError
+        return Vector2D(self.X - B.X, self.Y - B.Y)
+
+    def __iter__(self):
+        return iter([self.X, self.Y])
+
+    def __truediv__(self, B):
+        if isinstance(B, Vector2D):
+            return Vector2D(self.X / B.X, self.Y / B.Y)
+        elif isinstance(B, float) or isinstance(B, int):
+            return Vector2D(self.X / B, self.Y / B)
+        else:
+            return TypeError
+
+    @staticmethod
+    def min(A, B):
+        return Vector2D(min(A.X, B.X), min(A.Y, B.Y))
+
 
 @dataclass
 class Vector3D(Vector2D):
     Z: float
 
+    def __iter__(self):
+        return [self.X, self.Y, self.Z]
+
+    def __sub__(self, B):
+        if isinstance(B, Vector3D):
+            return Vector3D(self.X - B.X, self.Y - B.Y, self.Z - B.Z)
+        elif isinstance(B, Vector2D):
+            return Vector3D(self.X - B.X, self.Y - B.Y, self.Z)
+        else:
+            raise TypeError
+
+    def __add__(self, B):
+        if isinstance(B, Vector3D):
+            return Vector3D(self.X + B.X, self.Y + B.Y, self.Z + B.Z)
+        elif isinstance(B, Vector2D):
+            return Vector3D(self.X + B.X, self.Y + B.Y, self.Z)
+        else:
+            raise TypeError
+
+    def __truediv__(self, B):
+        if isinstance(B, Vector3D):
+            return Vector3D(self.X / B.X, self.Y / B.Y, self.Z / B.Z)
+        elif isinstance(B, float):
+            return Vector3D(self.X / B, self.Y / B, self.Z / B)
+        return super().__truediv__(B)
+
+    def ToVector2D(self):
+        return Vector2D(self.X, self.Y)
+
+
 @dataclass
-class Vector4D(Vector3D): 
+class Vector4D(Vector3D):
     W: float
+
 
 @dataclass
 class Rotator:
     Pitch: float
     Yaw: float
     Roll: float
+
     def __post_init__(self):
         self.ndarr = asarray(list(self.__dict__.values()))
 
+
 @dataclass
 class PathName:
-    ObjectName: str = ''
-    ObjectPath: str = ''
-    Outer: str = ''
+    ObjectName: str = ""
+    ObjectPath: str = ""
+    Outer: str = ""
     AssetType: str = field(init=False)
     AssetName: str = field(init=False)
     Context: str = field(init=False, default="N/A")
@@ -41,16 +103,17 @@ class PathName:
 
     def __post_init__(self):
         if self.ObjectName[-1] == "'":
-            self.AssetName = self.ObjectName.split("'")[-2].split('.')[-1]
+            self.AssetName = self.ObjectName.split("'")[-2].split(".")[-1]
         else:
-            self.AssetName = self.ObjectName.split('.')[-1]
+            self.AssetName = self.ObjectName.split(".")[-1]
 
         self.AssetType = self.ObjectName.split("'")[0]
 
-        if ':' in self.ObjectName:
-            parts = self.ObjectName.split(':')
+        if ":" in self.ObjectName:
+            parts = self.ObjectName.split(":")
             if len(parts) > 1:
                 self.Context = parts[1]
+
 
 @dataclass
 class Export:
@@ -58,13 +121,15 @@ class Export:
     Name: str
     Outer: Optional[str]
 
+
 ## Landscape Component
 @dataclass
 class Allocations:
     LayerInfo: PathName
     WeightmapTextureIndex: int
     WeightmapTextureChannel: int
-    
+
+
 @dataclass
 class LandscapeComponentProperties:
     ComponentSizeQuads: int
@@ -78,7 +143,8 @@ class LandscapeComponentProperties:
     WeightmapLayerAllocations: Optional[list[Allocations]] = field(default_factory=list)
     SectionBaseX: int = 0
     SectionBaseY: int = 0
-    RelativeLocation: Vector3D = Vector3D(0,0,0)
+    RelativeLocation: Vector3D = field(default_factory=lambda: Vector3D(0, 0, 0))
+
     def __post_init__(self):
         if self.SectionBaseX is None:
             self.SectionBaseX = 0
@@ -86,10 +152,12 @@ class LandscapeComponentProperties:
             self.SectionBaseY = 0
         self.SectionBase = Vector2D(self.SectionBaseX, self.SectionBaseY)
 
+
 @dataclass
 class LSCExport(Export):
     Properties: LandscapeComponentProperties
     Class: str = None
+
 
 ## Texture
 @dataclass
@@ -101,10 +169,12 @@ class TextureProperties:
     LODGroup: str
     SRGB: bool
 
+
 @dataclass
 class Texture2DExport(Export):
     Properties: TextureProperties
     Class: str = None
+
 
 ## Material instance
 @dataclass
@@ -113,11 +183,13 @@ class ParameterInfo:
     Association: str
     Index: int
 
+
 @dataclass
 class TerrainLayerWeightParameters:
-    WeightmapIndex: int 
+    WeightmapIndex: int
     bWeightBasedBlend: bool
     ParameterInfo: ParameterInfo
+
 
 @dataclass
 class ParameterValue:
@@ -127,25 +199,29 @@ class ParameterValue:
     A: float
     Hex: str
 
+
 @dataclass
 class VectorParameterValue:
     ParameterInfo: ParameterInfo
     ParameterValue: ParameterValue
 
+
 @dataclass
 class StaticParameters:
     TerrainLayerWeightParameters: list[TerrainLayerWeightParameters]
 
+
 @dataclass
 class LandscapeMaterialInstanceConstantProperties:
-    StaticParameters: Optional[dict[str,list[TerrainLayerWeightParameters]]]
+    StaticParameters: Optional[dict[str, list[TerrainLayerWeightParameters]]]
     VectorParameterValues: Optional[list[VectorParameterValue]]
 
 
 @dataclass
-class LMICExport(Export): #LandscapeMaterialInstanceConstant
+class LMICExport(Export):  # LandscapeMaterialInstanceConstant
     Properties: Optional[LandscapeMaterialInstanceConstantProperties]
     Class: str = None
+
 
 ## Landscape
 @dataclass
@@ -157,22 +233,26 @@ class LandscapeProperties:
     SubsectionSizeQuads: int
     NumSubsections: int
 
+
 @dataclass
 class LandscapeExport(Export):
-    Properties: Optional[LandscapeProperties]  = field(init=False, default=None)
+    Properties: Optional[LandscapeProperties] = field(init=False, default=None)
     Class: str = None
+
 
 ## Scene Component
 @dataclass
 class SceneComponentProperties:
     RelativeLocation: Vector3D
-    RelativeRotation: Rotator = Rotator(0,0,0)
-    RelativeScale3D: Vector3D = Vector3D(0,0,0)
+    RelativeRotation: Rotator = field(default_factory=lambda: Rotator(0, 0, 0))
+    RelativeScale3D: Vector3D = field(default_factory=lambda: Vector3D(1, 1, 1))
+
 
 @dataclass
 class SceneComponentExport(Export):
     Properties: SceneComponentProperties
     Class: str = None
+
 
 class ExportsEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -184,3 +264,185 @@ class ExportsEncoder(json.JSONEncoder):
             return obj.tolist()
         else:
             return super(ExportsEncoder, self).default(obj)
+
+
+@dataclass
+class AssetRef:
+    ObjectName: str
+    ObjectPath: str
+
+
+@dataclass
+class SplineConnection:
+    ControlPoint: AssetRef
+    TangentLen: float
+
+
+class CurveInterpMode(Enum):
+    # A straight line between two keypoint values.
+    CIM_Linear = 1
+    # A cubic-hermite curve between two keypoints, using Arrive/Leave tangents. These tangents will be automatically
+    # updated when points are moved, etc.  Tangents are unclamped and will plateau at curve start and end points.
+    CIM_CurveAuto = 2
+    # The out value is held constant until the next key, then will jump to that value.
+    CIM_Constant = 3
+    # A smooth curve just like CIM_Curve, but tangents are not automatically updated so you can have manual control over them (eg. in Curve Editor).
+    CIM_CurveUser = 4
+    # A curve like CIM_Curve, but the arrive and leave tangents are not forced to be the same, so you can create a 'corner' at this key.
+    CIM_CurveBreak = 5
+    # A cubic-hermite curve between two keypoints, using Arrive/Leave tangents. These tangents will be automatically
+    # updated when points are moved, etc.  Tangents are clamped and will plateau at curve start and end points.
+    CIM_CurveAutoClamped = 6
+    # Invalid or unknown curve type.
+    CIM_Unknown = 7
+
+
+@dataclass
+class SplinePoint:
+    InVal: float = 0.0
+    OutVal: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    ArriveTangent: Vector3D = field(default_factory=lambda: Vector3D(0, 0, 0))
+    LeaveTangent: Vector3D = field(default_factory=lambda: Vector3D(0, 0, 0))
+    InterpMode: CurveInterpMode | str = CurveInterpMode.CIM_Unknown
+
+    def __post_init__(self):
+        if isinstance(self.InterpMode, str):
+            interp = re.sub(r"^.*::", "", self.InterpMode)
+            self.InterpMode = CurveInterpMode[interp]
+
+
+@dataclass
+class SplineInfoData:
+    Points: list[SplinePoint]
+
+
+@dataclass
+class SegmentPoint:
+    Center: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    Left: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    Right: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    FalloffLeft: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    FalloffRight: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    LayerLeft: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    LayerRight: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    LayerFalloffLeft: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    LayerFalloffRight: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    StartEndFalloff: float = 0.0
+
+
+@dataclass
+class SplineBounds:
+    Min: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    Max: Vector3D = field(
+        default_factory=lambda: field(default_factory=lambda: Vector3D(0, 0, 0))
+    )
+    IsValid: int = 0
+
+
+@dataclass
+class SplineParameter:
+    StartPos: Vector3D
+    StartTangent: Vector3D
+    StartScale: Optional[Vector2D]
+    StartOffset: Optional[Vector2D]
+    EndPos: Vector3D
+    EndTangent: Vector3D
+    EndScale: Optional[Vector2D]
+    EndOffset: Optional[Vector2D]
+
+
+class ECollisionEnabled(Enum):
+    NoCollision = 0
+    QueryOnly = 1
+    PhysicsOnly = 2
+    QueryAndPhysics = 3
+    ProbeOnly = 4
+    QueryAndProbe = 5
+
+
+@dataclass
+class MeshBodyInstance:
+    CollisionEnabled: Optional[ECollisionEnabled | str]
+    CollisionProfileName: Optional[str]
+    bAutoWeld: Optional[bool]
+
+    def __post_init__(self):
+        if isinstance(self.CollisionEnabled, str):
+            coll = re.sub(r"^.*::", "", self.CollisionEnabled)
+            self.CollisionEnabled = ECollisionEnabled[coll]
+
+
+@dataclass
+class SplineMeshComponentProperty:
+    SplineParams: SplineParameter
+    CachedMeshBodySetupGuid: str
+    BodySetup: AssetRef
+    bNeverNeedsCookedCollisionData: Optional[bool]
+    StaticMesh: Optional[AssetRef]
+    CastShadow: Optional[bool]
+    BodyInstance: Optional[MeshBodyInstance]
+    bHasNoStreamableTextures: Optional[bool]
+    AttachParent: AssetRef
+    RelativeLocation: Vector3D
+    bVisible: Optional[bool]
+    bHiddenInGame: Optional[bool]
+
+
+@dataclass
+class SplineMeshComponent(Export):
+    Class: str
+    Flags: str
+    Properties: SplineMeshComponentProperty
+
+
+@dataclass
+class SplineSegmentProperty:
+    ConnectionStart: Optional[SplineConnection]
+    ConnectionEnd: Optional[SplineConnection]
+    SplineInfo: SplineInfoData
+    Points: list[SegmentPoint]
+    Bounds: SplineBounds
+    LocalMeshComponents: list[AssetRef | SplineMeshComponent]
+
+
+@dataclass
+class LandscapeSplineSegment(Export):
+    Class: str
+    Flags: str
+    Properties: SplineSegmentProperty
+
+
+@dataclass
+class SplineComponentProperty:
+    ControlPoints: list[AssetRef]
+    Segments: list[AssetRef | LandscapeSplineSegment]
+    RelativeLocation: Vector3D
+
+
+@dataclass
+class LandscapeSplinesComponent(Export):
+    Class: str
+    Flags: str
+    Properties: SplineComponentProperty
